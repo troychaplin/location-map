48344acbaaaaa66b2840bae93485818c09338e9b
troychaplin <troy.chaplin@gmail.com>
2025-03-31 14:26:03 -0400

    change: formatting

--- Functions/RestApi.php
+++ Functions/RestApi.php
@@ -2,43 +2,49 @@
 
 namespace Location_Map;
 
-if (!defined('ABSPATH')) {
-    exit; // Exit if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
 }
 
 /**
  * Location Map REST API Class
  */
 class RestApi {
-    /**
-     * Constructor
-     */
-    public function __construct() {
-        add_action('rest_api_init', array($this, 'register_routes'));
-    }
+	/**
+	 * Constructor
+	 */
+	public function __construct() {
+		add_action( 'rest_api_init', array( $this, 'register_routes' ) );
+	}
 
-    /**
-     * Register REST API routes
-     */
-    public function register_routes() {
-        register_rest_route('wp/v2/location-map', '/settings', array(
-            'methods' => 'GET',
-            'callback' => array($this, 'get_settings'),
-            'permission_callback' => function () {
-                return current_user_can('edit_posts');
-            }
-        ));
-    }
+	/**
+	 * Register REST API routes
+	 */
+	public function register_routes() {
+		register_rest_route(
+			'wp/v2/location-map',
+			'/settings',
+			array(
+				'methods'             => 'GET',
+				'callback'            => array( $this, 'get_settings' ),
+				'permission_callback' => function () {
+					return current_user_can( 'edit_posts' );
+				},
+			)
+		);
+	}
 
-    /**
-     * Get settings
-     *
-     * @return WP_REST_Response
-     */
-    public function get_settings() {
-        $options = get_option('location_map_settings');
-        return rest_ensure_response(array(
-            'google_maps_api_key' => isset($options['google_maps_api_key']) ? $options['google_maps_api_key'] : ''
-        ));
-    }
+	/**
+	 * Get settings
+	 *
+	 * @return WP_REST_Response
+	 */
+	public function get_settings() {
+		$options = get_option( 'location_map_settings' );
+		return rest_ensure_response(
+			array(
+				'google_maps_api_key' => isset( $options['google_maps_api_key'] ) ? $options['google_maps_api_key'] : '',
+			)
+		);
+	}
 }
--- build/location-map/index.asset.php
+++ build/location-map/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '8b6942da120dc83f72df');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'a9641d092924624e3124');
